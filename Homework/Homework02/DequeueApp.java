625cbfcb171b027fdb3429cdf16208ed8b4cd72e
avasqu27 <35350821+avasqu27@users.noreply.github.com>
2018-10-24 23:42:38 -0700

    Update DequeueApp.java

--- Homework/Homework02/DequeueApp.java
+++ Homework/Homework02/DequeueApp.java
@@ -1,5 +1,5 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * File name  :  DequeueApp.java
+ * File name  :  DequeApp.java
  * Purpose    :  Gain practice with packages
  * @author    :  Ale Vasquez
  * @author    :  Bob (R. Ventura)
@@ -10,73 +10,80 @@
  * Warnings   :  None
  *
  *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-class DequeueApp {
+class DequeApp {
 
    public static void main(String[] args) {
 
-     Dequeue theDequeue = new Dequeue(5); // dequeue holds 5 items
-     System.out.println("\nDequeue is going to insert 10,20, 30, and 40 to the left. ");
-     theDequeue.insertLeft(10);           // insert 4 items
-     theDequeue.insertLeft(20);
-     theDequeue.insertLeft(30);
-     theDequeue.insertLeft(40);
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
-     System.out.println("\nDequeue is going to removeLeft() 3 times. ");
-     theDequeue.removeLeft();             // remove 3 items
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
-     theDequeue.removeLeft();             //    (10, 20, 30)
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
-     theDequeue.removeLeft();
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
-     System.out.println("\nDequeue is going to insert 50, 60, 70, and 80 to the right " );
-     theDequeue.insertRight(50);           // insert 4 more items
-     theDequeue.insertRight(60);           //    (wraps around)
-     theDequeue.insertRight(70);
-     theDequeue.insertRight(80);
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
-     System.out.println("\nDequeue is now going to delete items: repeated 3 times: ");
-     theDequeue.removeRight();
-     theDequeue.removeRight();
-     theDequeue.removeRight();
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
-     System.out.println("\nDequeue is going to insert 45,0, 32, and 45 to the left. ");
-     theDequeue.insertLeft(45);           // insert 4 items
-     theDequeue.insertLeft(0);
-     theDequeue.insertLeft(32);
-     theDequeue.insertLeft(45);
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
-     System.out.println("\nDequeue is going to removeLeft() 3 times. ");
-     theDequeue.removeLeft();             // remove 3 items
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
-     theDequeue.removeLeft();             //    (10, 20, 30)
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
-     theDequeue.removeLeft();
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
-     System.out.println("\nDequeue is going to insert 66, 888, 10, and 99 to the right " );
-     theDequeue.insertRight(66);           // insert 4 more items
-     theDequeue.insertRight(888);           //    (wraps around)
-     theDequeue.insertRight(10);
-     theDequeue.insertRight(99);
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
-     System.out.println("\nDequeue is now going to delete items from the right: repeated 4 times: ");
-     theDequeue.removeRight();
-     theDequeue.removeRight();
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
-     theDequeue.removeRight();
-     theDequeue.removeRight();
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
+     Deque theDeque = new Deque(5); // Deque holds 5 items
+     System.out.println("\nDeque is going to insert 10,20, 30, and 40 to the left. ");
+     theDeque.insertLeft(10);           // insert 4 items
+     theDeque.insertLeft(20);
+     theDeque.insertLeft(30);
+     theDeque.insertLeft(40);
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     System.out.println("\nDeque is going to removeLeft() 3 times. ");
+     theDeque.removeLeft();             // remove 3 items
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     theDeque.removeLeft();             //    (10, 20, 30)
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     theDeque.removeLeft();
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     System.out.println("\nDeque is going to insert 50, 60, 70, and 80 to the right " );
+     theDeque.insertRight(50);           // insert 4 more items
+     theDeque.insertRight(60);           //    (wraps around)
+     theDeque.insertRight(70);
+     theDeque.insertRight(80);
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     System.out.println("\nIs the deque full though? " + theDeque.isFull());
+     System.out.println("\nDeque is now going to delete items: repeated 3 times: ");
+     theDeque.removeRight();
+     theDeque.removeRight();
+     theDeque.removeRight();
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     System.out.println("\nDeque is going to insert 45,0, 32, and 45 to the left. ");
+     theDeque.insertLeft(45);           // insert 4 items
+     theDeque.insertLeft(0);
+     theDeque.insertLeft(32);
+     theDeque.insertLeft(45);
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     System.out.println("\nDeque is going to removeLeft() 3 times. ");
+     theDeque.removeLeft();             // remove 3 items
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     theDeque.removeLeft();             //    (10, 20, 30)
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     theDeque.removeLeft();
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     System.out.println("\nDeque is going to insert 66, 888, 10, and 99 to the right " );
+     theDeque.insertRight(66);           // insert 4 more items
+     theDeque.insertRight(888);           //    (wraps around)
+     theDeque.insertRight(10);
+     theDeque.insertRight(99);
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     System.out.println("\nIs the deque empty though? " + theDeque.isEmpty());
+     System.out.println("\nDeque is now going to delete items from the right: repeated 5 times: ");
+     theDeque.removeRight();
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     theDeque.removeRight();
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     theDeque.removeRight();
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     theDeque.removeRight();
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     theDeque.removeRight();
+     System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
+     System.out.println("\nIs the deque empty though? " + theDeque.isEmpty());
 
-     int x = theDequeue.queArray.length;
+     int x = theDeque.queArray.length;
      /*
      while (x >= 0 ) {
-  //   while( !theDequeue.isEmpty() ) {     // remove and display all items
-       long n = theDequeue.removeRight();      // (40, 50, 60, 70, 80)
+  //   while( !theDeque.isEmpty() ) {     // remove and display all items
+       long n = theDeque.removeRight();      // (40, 50, 60, 70, 80)
        System.out.println(n);
-       System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
+       System.out.println("\nDeque is currently: " + theDeque.frontQ.toString());
        x--;
      }
      */
-     System.out.println("\nDequeue is currently: " + theDequeue.frontQ.toString());
+     System.out.println("\nOur final Deque is: " + theDeque.frontQ.toString());
      System.out.println("");
   } // end main()
-} // end class DequeueApp
+} // end class DequeApp
