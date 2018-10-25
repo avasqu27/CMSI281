33b65d1e4bad0abe04fcfdfc682c5329d8ca0708
avasqu27 <35350821+avasqu27@users.noreply.github.com>
2018-10-24 23:45:29 -0700

    Update Queue.java

--- Homework/Homework02/Queue.java
+++ Homework/Homework02/Queue.java
@@ -1,5 +1,5 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * File name  :  Queue.java
+ * File name  :  QueueApp.java
  * Purpose    :  Gain practice with packages
  * @author    :  Ale Vasquez
  * @author    :  Bob (R. Ventura)
@@ -10,91 +10,30 @@
  * Warnings   :  None
  *
  *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-public class Queue {
+class QueueApp {
 
-  public   int    maxSize;
-  public   long[] queArray;
-  public   int    front;
-  public   int    rear;
-  public   int    nItems;
+   public static void main(String[] args) {
 
- // our queue should dis[play contents of the queue.
-  // Show the queue contents from the first item (inserted first) to the last
-  // no indication to whether sequence is broken
-  // must be tested with displaying one item
-  // must be tested with displaying no items
-  //--------------------------------------------------------------
-
-  public Queue(int s) { // constructor
-    maxSize = s;
-    queArray = new long[maxSize];
-    front = 0;
-    rear = -1;
-    nItems = 0;
-  }
-//--------------------------------------------------------------
-  /*
-    @param long value to be inserted
-    @return void; adda value to end of queue
-  */
-  public void insert(long j) {               // put item at rear of queue
-    if(rear == maxSize-1){                   // deal with wraparound
-      rear = -1;
-    }
-    queArray[++rear] = j;                   // increment rear and insert
-    nItems++;                               // one mre item
-  }
-//--------------------------------------------------------------\
-  /*
-    @return long; removes item in front of queue
-  */
-  public long remove() {// take item from front of queue
-    long temp = queArray[front++]; // get value and incr front
-    if(front == maxSize){ // deal with wraparound
-      front = 0;
-    }
-    nItems--; // one less item
-    return temp;
-  }
-//--------------------------------------------------------------
-  /*
-    @return long; looks at front of queue
-  */
-   public long peekFront() {     // peek at front of queue
-     return queArray[front];
-   }
-//--------------------------------------------------------------
-  /*
-    @return boolean; returns true if queue is empty
-  */
-   public boolean isEmpty() {     // true if queue is empty {
-     return (nItems==0);
-   }
-//--------------------------------------------------------------
-  /*
-    @return boolean; returns false if queue is empty
-  */
-   public boolean isFull() {     // true if queue is full {
-     return (nItems==maxSize);
-    }
-//--------------------------------------------------------------
-  /*
-    @return int; returns number of items in queue
-  */
-  public int size() {           // number of items in queue {
-     return nItems;
-  }
-//--------------------------------------------------------------
-  public String toString() {
-    String s = "[ ";
-    for (int i = 0; i < queArray.length; i++) {
-      s = s + queArray[i] + " ";
-    }
-    s = s + "]";
-    return s;
-  }
-//--------------------------------------------------------------
-
- // end class Queue
-
-}
+     Queue theQueue = new Queue(5); // queue holds 5 items
+     theQueue.insert(10);           // insert 4 items
+     theQueue.insert(20);
+     theQueue.insert(30);
+     theQueue.insert(40);
+     System.out.println("\nQueue is currently: " + theQueue.toString());
+     theQueue.remove();             // remove 3 items
+     theQueue.remove();             //    (10, 20, 30)
+     theQueue.remove();
+     theQueue.insert(50);           // insert 4 more items
+     theQueue.insert(60);           //    (wraps around)
+     theQueue.insert(70);
+     theQueue.insert(80);
+     System.out.println("\nQueue is currently: " + theQueue.toString());
+     System.out.println("\nQueue is now going to delete items and display items deleted ");
+     while( !theQueue.isEmpty() ) {     // remove and display all items
+       long n = theQueue.remove();      // (40, 50, 60, 70, 80)
+       System.out.print(n);
+       System.out.print(" ");
+     }
+     System.out.println("");
+  } // end main()
+} // end class QueueApp
